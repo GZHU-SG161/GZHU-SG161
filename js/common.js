34cf3a0c4941abd87a5d44e317b977f8e41cb7c6
GZHU-SG161 <68576393+GZHU-SG161@users.noreply.github.com>
2020-07-21 11:42:20 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,27 +1,27 @@
-// window.addEventListener('load', function(){
+window.addEventListener('load', function(){
     
-//     var roomNo = document.querySelector('.roomNo');
-//     var mine = document.querySelector('.mine');
-//     var nav = document.querySelector('.nav');
-//     var dorm = nav.querySelector('.dorm');
-//     var myPro = nav.querySelector('.myPro');
+    var roomNo = document.querySelector('.roomNo');
+    var mine = document.querySelector('.mine');
+    var nav = document.querySelector('.nav');
+    var dorm = nav.querySelector('.dorm');
+    var myPro = nav.querySelector('.myPro');
 
-//     dorm.addEventListener('mouseenter', function(){
-//         roomNo.style.display = 'block';
-//     })
-//     roomNo.addEventListener('mouseenter', function(){
-//         roomNo.style.display = 'block';
-//     })
-//     dorm.addEventListener('mouseleave', function(){
-//         roomNo.style.display = 'none';
-//     })
-//     roomNo.addEventListener('mouseleave', function(){
-//         roomNo.style.display = 'none';
-//     })
-//     myPro.addEventListener('mouseenter', function(){
-//         mine.style.display = 'block';
-//     })
-//     myPro.addEventListener('mouseleave', function(){
-//         mine.style.display = 'none';
-//     })
-// })
+    dorm.addEventListener('mouseenter', function(){
+        roomNo.style.display = 'block';
+    })
+    roomNo.addEventListener('mouseenter', function(){
+        roomNo.style.display = 'block';
+    })
+    dorm.addEventListener('mouseleave', function(){
+        roomNo.style.display = 'none';
+    })
+    roomNo.addEventListener('mouseleave', function(){
+        roomNo.style.display = 'none';
+    })
+    myPro.addEventListener('mouseenter', function(){
+        mine.style.display = 'block';
+    })
+    myPro.addEventListener('mouseleave', function(){
+        mine.style.display = 'none';
+    })
+})
